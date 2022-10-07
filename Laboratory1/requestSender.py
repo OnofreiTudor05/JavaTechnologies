58c9d92dd7404b87b75ab28166413b3a87049689
OnofreiTudor2A3 <38893360+OnofreiTudor05@users.noreply.github.com>
2022-10-07 23:38:48 +0300

    Updated Python Script & Log File

--- Laboratory1/requestSender.py
+++ Laboratory1/requestSender.py
@@ -1,8 +1,18 @@
 import urllib.request
 import urllib.parse
+import threading
 
-try:
-    x = urllib.request.urlopen('http://localhost:8080/Laboratory1/wordGen?word=elephant&size=0')
-    print(x.read().decode('utf8'))
-except Exception as e:
-    print(str(e))
+
+def thread_function():
+    try:
+        x = urllib.request.urlopen('http://localhost:8080/Laboratory1/wordGen?word=elephant&size=0')
+        word_list = x.read().decode('utf8')
+        print(word_list)
+    except Exception as e:
+        print(str(e))
+
+
+if __name__ == "__main__":
+    for i in range(1, 20):
+        new_thread = threading.Thread(target=thread_function())
+        new_thread.start()
